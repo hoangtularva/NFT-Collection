c4211c8215a02fcd0052c3e3ca47ffc657e04898
Tran Hoang Tu <50492205+hoangtularva@users.noreply.github.com>
2023-02-06 00:41:11 +0700

    build an NFT Collection

---
+++ hardhat-tutorial/contracts/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xEbCADe44416e1D46609Cc931130F7DE76563c4B7";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
